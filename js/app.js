e0d91b1a60d86b4d2d5c6d348c3e09ea640e51e2
Jacob Anderson <1anderson2jacob@gmail.com>
2018-10-16 10:10:29 -0700

    finished the pike object

--- js/app.js
+++ js/app.js
@@ -1,41 +1,52 @@
 'use strict';
+var hours = ['6am', '7am','8am','9am','10am','11am','12pm','1pm','2pm','3pm','4pm','5pm', '6pm', '7pm']
 
 var pike = {
-  name : ['1st and Pike'],
-  minCust : 23, //min cust each hour
-  maxCust : 65, //max cust each hour
-  avgCookies : 6.3 //avg cookies per customer
-  
+  locationName : ['First and Pike'],
+  minCustPerHour : 23, //min cust each hour
+  maxCustPerHour : 65, //max cust each hour
+  avgCookies : 6.3, //avg cookies per customer
+  customersEachHour: [],
+  cookiesEachHour: [],
+  totalDailyCookies: 0
 
-  // [] cookies
-  // 	method to calculate and populate
-  // [] customers // use to calculate cookies[]
-  // 	method to calculate/populate
-  // daily total per location
   // render()
-
 }
 
+pike.calcCustomersEachHour = function() {
+  for (var i = 0; i < hours.length; i++) {
+    var randomCustomers = random(this.minCustPerHour, this.maxCustPerHour);
+    this.customersEachHour[i] = randomCustomers;
+  }
+}
+
+pike.calcCookiesEachHour = function() {
+  pike.calcCustomersEachHour();
+  for (var i = 0; i < hours.length; i++) {
+    var hourlyCookies = Math.ceil(this.customersEachHour[i] * this.avgCookies);
+    this.cookiesEachHour[i] = hourlyCookies;
+    this.totalDailyCookies += hourlyCookies;
+  }
+}
+pike.render = function() {
+  this.calcCookiesEachHour();
+  var pikeUl = document.getElementById('pike');
+  console.log(pikeUl, '<pikeUl>');
+  for(var i = 0; i < hours.length; i++) {
+    console.log('render for loop working');
+    //create element
+    var liEl = document.createElement('li');
+    //give content
+    liEl.textContent = `${hours[i]}: ${this.cookiesEachHour[i]} cookies`;
+    //append
+    pikeUl.appendChild(liEl);
+  }
+  liEl = document.createElement('li');
+  liEl.textContent = `Total: ${this.totalDailyCookies} cookies`;
+  pikeUl.appendChild(liEl);
+}
 //random cookies per customer
-pike.randCustMath = function (min, max) {
-  min = Math.ceil(min);
-  max = Math.floor(max);
-  return Math.floor(Math.random() * (max - min + 1)) + min;
+function random(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min; //from mdn docs
 }
 
-pike.customers = function() {
-  var numCust = [];
-  //pike.randCustMath
-  for (var i = 0; i < 15; i++) {
-    numCust.push(pike.randCustMath(pike.minCust, pike.maxCust));
-  }
-
-  return numCust;
-}
-
-pike.cookies = function(customers()) {
-  var numCookies = [];
-  for (var i = 0; i < 15; i++) {
-    
-  }
-}
